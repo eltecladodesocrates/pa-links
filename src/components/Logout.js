b6b9d2b0d7795b05f58b4e9c9643230e35db5a44
Luis Rodriguez <rp@luiss-air.myfiosgateway.com>
2021-09-11 05:53:07 -0400

    logout redirection

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,13 +1,16 @@
 import React, { useContext } from 'react'
 import { startLogOutUser } from '../actions/user'
 import { AuthContext } from '../context/auth'
+import { useHistory } from 'react-router-dom'
 
 const Logout = () => {
 
     const {user, userDispatch} = useContext(AuthContext)
+    const history = useHistory()
 
     const handleLogOut = () => {
         startLogOutUser(userDispatch)
+        history.push('/dashboard')
     }
 
     return (
